b2ecc246c73828b1088cd7eb77c33d731888e96a
Cloufish <bartmok17@gmail.com>
2021-04-02 19:09:35 +0200

    Added colors

--- full_web.sh
+++ full_web.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 usage(){
 
@@ -45,14 +45,31 @@ while getopts d:a:u:p OPTION; do
 	esac
 done
 
+redCl="\e[0;31m\033[1m"
+greenCl="\e[0;32m\033[1m"
+yellowCl="\e[0;33m\033[1m"
+blueCl="\e[0;34m\033[1m"
+magentCl="\e[0;35m\033[1m"
+cyanCl="\e[0;36m\033[1m"
+endCl="\033[0m\e[0m"
+
+
+echo -e "${redCl} INITIALIZATION.sh ${endCl}"
 initialization.sh -d "$domain" -a "$ASN" -u "$USER_EXEC"
 get-other-root-domains.sh -d "$domain" -u "$USER_EXEC" &
+echo -e "${greenCl} GET-TECHNOLOGIES.sh ${endCl}"
 get-technologies.sh -d "$domain" -u "$USER_EXEC"
+echo -e "${yellowCl} GET-SUBDOMAINS-PASSIVELY.sh ${endCl}"
 get-subdomains-passively.sh -d "$domain" -u "$USER_EXEC"
+echo -e "${blueCl} GET-ALIVE-SUBDOMAINS.sh ${endCl}"
 get-alive-subdomains.sh -d "$domain" -u "$USER_EXEC"
+echo -e "${magentCl} GET-NOT-ALIVE-SUBDOMAINS.sh ${endCl}"
 get-not-alive-subdomains-ip.sh -d "$domain" -u "$USER_EXEC"
+echo -e "${cyanCl} EXTRACTING-JAVASCRIPT.sh ${endCl}"
 extracting-javascript.sh -d "$domain" -u "$USER_EXEC"
 if [ $NO_BRUTE -ne 1 ]; then
+	echo -e "${redCl} BRUTING-NOT-ALIVE-SUBDOMAINS.sh ${endCl}"
 	bruting-not-alive-subdomains-ip.sh -d "$domain" -u "$USER_EXEC"
+	echo -e "${redCl} BRUTING-NOT-ALIVE-SUBDOMAINS.sh ${endCl}"
 	bruting-alive-subdomains.sh -d "$domain" -u "$USER_EXEC"
 fi
