e6d5999065094a1ffab4cf2f307bd3f1a08cb371
Cloufish <32965886+Cloufish@users.noreply.github.com>
2021-04-12 18:14:16 +0200

    Update slack-bot.sh

--- Web/slack-bot.sh
+++ Web/slack-bot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SLACK_WEBHOOK_URL=$(echo $SLACK_WEBHOOK_URL)
+SLACK_WEBHOOK_URL=$("${SLACK_WEBHOOK_URL}")
 function push {
   RESPONSE=$(curl -X POST -H 'Content-type: application/json' --data '{"text":${1}' "${YOUR_WEBHOOK_URL}")
   echo $RESPONSE
