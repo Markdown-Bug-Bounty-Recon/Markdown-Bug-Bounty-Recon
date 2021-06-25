98285155e34fb1772ecd938c4eb5c8aacb0d4e98
Cloufish <cloufish@protonmail.com>
2021-06-25 14:35:09 +0200

    Add frida server script

---
+++ Mobile/getting_frida_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+
+newest_frida_version=$(frida --version)
+phone_architecture=$(adb shell getprop ro.product.cpu.abi)
+
+wget https://github.com/frida/frida/releases/download/"${newest_frida_version}"/frida-server-"${newest_frida_version}"-android-"${phone_architecture}".xz
+
+unxz frida-server-"${newest_frida_version}"-android-arm.xz
+
+name_of_the_frida_server=frida-server-"${newest_frida_version}"-android-"${phone_architecture}"
+
+
+adb root
+
+adb push "${PWD}""${name_of_the_frida_server}" /data/local/frida
+
+adb shell /data/local/frida &
